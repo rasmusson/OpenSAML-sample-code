7081c2227941bd9f2384982a11e526a91ba3d3f7
Stefan Rasmusson <rasmusson.stefan@gmail.com>
2021-10-10 09:17:06 +0200

    formating

--- getting-started-authnrequest/src/main/java/com/samlsecurity/opensamlSamples/gettingStartedAuthnrequest/GenAuthnRequest.java
+++ getting-started-authnrequest/src/main/java/com/samlsecurity/opensamlSamples/gettingStartedAuthnrequest/GenAuthnRequest.java
@@ -9,14 +9,10 @@ import org.opensaml.core.config.InitializationException;
 import org.opensaml.core.config.InitializationService;
 import org.opensaml.core.xml.config.XMLObjectProviderRegistry;
 import org.opensaml.saml.common.xml.SAMLConstants;
-import org.opensaml.saml.saml2.core.AuthnContext;
-import org.opensaml.saml.saml2.core.AuthnContextClassRef;
-import org.opensaml.saml.saml2.core.AuthnContextComparisonTypeEnumeration;
 import org.opensaml.saml.saml2.core.AuthnRequest;
 import org.opensaml.saml.saml2.core.Issuer;
 import org.opensaml.saml.saml2.core.NameIDPolicy;
 import org.opensaml.saml.saml2.core.NameIDType;
-import org.opensaml.saml.saml2.core.RequestedAuthnContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -28,23 +24,22 @@ public class GenAuthnRequest {
 	private static final String IPD_SSO_DESTINATION = "https://idp.example.com/singleSingOnService";
 	private static final String SP_ASSERTION_CONSUMER_SERVICE_URL = "https://sp.example.com/assertionConsumerService";
 	private static final String SP_ISSUED_ID = "IssuerEntityId";
-    private static Logger logger = LoggerFactory.getLogger(GenAuthnRequest.class);
+	private static Logger logger = LoggerFactory.getLogger(GenAuthnRequest.class);
 
-	
 	public static void main(String[] args) {
-		
-		initOpenSAML();	
+
+		initOpenSAML();
 		AuthnRequest authnRequest = buildAuthnRequest();
-        OpenSAMLUtils.logSAMLObject(authnRequest);
+		OpenSAMLUtils.logSAMLObject(authnRequest);
 	}
 
 	private static void initOpenSAML() {
 		XMLObjectProviderRegistry registry = new XMLObjectProviderRegistry();
 		ConfigurationService.register(XMLObjectProviderRegistry.class, registry);
-		    
+
 		registry.setParserPool(getParserPool());
 		try {
-		InitializationService.initialize();
+			InitializationService.initialize();
 		} catch (InitializationException e) {
 			logger.error(e.getMessage(), e);
 		}
@@ -52,62 +47,60 @@ public class GenAuthnRequest {
 
 	private static ParserPool getParserPool() {
 		BasicParserPool parserPool = new BasicParserPool();
-        parserPool.setMaxPoolSize(100);
-        parserPool.setCoalescing(true);
-        parserPool.setIgnoreComments(true);
-        parserPool.setIgnoreElementContentWhitespace(true);
-        parserPool.setNamespaceAware(true);
-        parserPool.setExpandEntityReferences(false);
-        parserPool.setXincludeAware(false);
+		parserPool.setMaxPoolSize(100);
+		parserPool.setCoalescing(true);
+		parserPool.setIgnoreComments(true);
+		parserPool.setIgnoreElementContentWhitespace(true);
+		parserPool.setNamespaceAware(true);
+		parserPool.setExpandEntityReferences(false);
+		parserPool.setXincludeAware(false);
 
-        
-        final Map<String, Boolean> features = new HashMap<String, Boolean>();
-        features.put("http://xml.org/sax/features/external-general-entities", Boolean.FALSE);
-        features.put("http://xml.org/sax/features/external-parameter-entities", Boolean.FALSE);
-        features.put("http://apache.org/xml/features/disallow-doctype-decl", Boolean.TRUE);
-        features.put("http://apache.org/xml/features/validation/schema/normalized-value", Boolean.FALSE);
-        features.put("http://javax.xml.XMLConstants/feature/secure-processing", Boolean.TRUE);
+		final Map<String, Boolean> features = new HashMap<String, Boolean>();
+		features.put("http://xml.org/sax/features/external-general-entities", Boolean.FALSE);
+		features.put("http://xml.org/sax/features/external-parameter-entities", Boolean.FALSE);
+		features.put("http://apache.org/xml/features/disallow-doctype-decl", Boolean.TRUE);
+		features.put("http://apache.org/xml/features/validation/schema/normalized-value", Boolean.FALSE);
+		features.put("http://javax.xml.XMLConstants/feature/secure-processing", Boolean.TRUE);
 
-        
-        parserPool.setBuilderFeatures(features);
-        
-        parserPool.setBuilderAttributes(new HashMap<String, Object>());
+		parserPool.setBuilderFeatures(features);
 
-        try {
+		parserPool.setBuilderAttributes(new HashMap<String, Object>());
+
+		try {
 			parserPool.initialize();
 		} catch (ComponentInitializationException e) {
 			logger.error(e.getMessage(), e);
 		}
-              
-        return parserPool;
+
+		return parserPool;
 	}
-	
+
 	private static AuthnRequest buildAuthnRequest() {
 		AuthnRequest authnRequest = OpenSAMLUtils.buildSAMLObject(AuthnRequest.class);
-        authnRequest.setIssueInstant(Instant.now());
-        authnRequest.setDestination(IPD_SSO_DESTINATION);
-        authnRequest.setProtocolBinding(SAMLConstants.SAML2_ARTIFACT_BINDING_URI);
-        authnRequest.setAssertionConsumerServiceURL(SP_ASSERTION_CONSUMER_SERVICE_URL);
-        authnRequest.setID(OpenSAMLUtils.generateSecureRandomId());
-        authnRequest.setIssuer(buildIssuer());
-        authnRequest.setNameIDPolicy(buildNameIdPolicy());
-        
-        return authnRequest;
+		authnRequest.setIssueInstant(Instant.now());
+		authnRequest.setDestination(IPD_SSO_DESTINATION);
+		authnRequest.setProtocolBinding(SAMLConstants.SAML2_ARTIFACT_BINDING_URI);
+		authnRequest.setAssertionConsumerServiceURL(SP_ASSERTION_CONSUMER_SERVICE_URL);
+		authnRequest.setID(OpenSAMLUtils.generateSecureRandomId());
+		authnRequest.setIssuer(buildIssuer());
+		authnRequest.setNameIDPolicy(buildNameIdPolicy());
+
+		return authnRequest;
 	}
-	
+
 	private static NameIDPolicy buildNameIdPolicy() {
-        NameIDPolicy nameIDPolicy = OpenSAMLUtils.buildSAMLObject(NameIDPolicy.class);
-        nameIDPolicy.setAllowCreate(true);
-        nameIDPolicy.setFormat(NameIDType.TRANSIENT);
+		NameIDPolicy nameIDPolicy = OpenSAMLUtils.buildSAMLObject(NameIDPolicy.class);
+		nameIDPolicy.setAllowCreate(true);
+		nameIDPolicy.setFormat(NameIDType.TRANSIENT);
 
-        return nameIDPolicy;
-    }
+		return nameIDPolicy;
+	}
 
-    private static Issuer buildIssuer() {
-        Issuer issuer = OpenSAMLUtils.buildSAMLObject(Issuer.class);
-        issuer.setValue(SP_ISSUED_ID);
+	private static Issuer buildIssuer() {
+		Issuer issuer = OpenSAMLUtils.buildSAMLObject(Issuer.class);
+		issuer.setValue(SP_ISSUED_ID);
 
-        return issuer;
-    }
+		return issuer;
+	}
 
 }
