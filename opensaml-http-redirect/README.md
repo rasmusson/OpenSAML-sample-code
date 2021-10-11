c38b1d619b6928b7f1cb70646442e05b3fcfe828
Stefan Rasmusson <rasmusson.stefan@gmail.com>
2021-10-11 21:27:17 +0200

    first commit redirect sample

---
+++ opensaml-http-redirect/README.md
@@ -0,0 +1,19 @@
+# Demo of SP and IdP interacting using OpenSAML 4
+This code shows a simulation of how a SP and a IdP might communicate using SAML. Variations on this demo is used exensivley in the books [A Guide to OpenSAML V2](https://payhip.com/b/odEY) and [A Guide to OpenSAML V3](https://payhip.com/b/41Tw).
+
+The demo code sets up several servlets acting as different endpoints on SP, IdP and business application.
+
+The demo acts a good overview guide to OpenSAML demonstrating:
+* Building, singing and sending a AuthnRequest using HTTP Redirect binding
+* Parsing and verifying signatures on AuthnRequest
+* Sending a Response message using the Artifact binding
+* Encrypting and decryoting a Assertion
+
+## Runing the code
+Clone the code, navigate to the folder for this sample and run: 
+```
+mvn tomcat:run
+```
+
+Then open a browser and navigate to http://localhost:8080/opensaml4-webprofile-demo/app/appservlet
+All SAML messages used during the communication are logged to the console.
---
+++ opensaml-http-redirect/src/main/java/no/steras/opensamlSamples/httpRedirect/ReceiverServlet.java
@@ -0,0 +1,58 @@
+package no.steras.opensamlSamples.httpRedirect;
+
+import java.io.IOException;
+import java.io.Writer;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.opensaml.messaging.context.MessageContext;
+import org.opensaml.messaging.decoder.MessageDecodingException;
+import org.opensaml.saml.saml2.binding.decoding.impl.HTTPRedirectDeflateDecoder;
+import org.opensaml.saml.saml2.core.AuthnRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.shibboleth.utilities.java.support.component.ComponentInitializationException;
+
+public class ReceiverServlet extends HttpServlet {
+	private static Logger logger = LoggerFactory.getLogger(ReceiverServlet.class);
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		HTTPRedirectDeflateDecoder decoder = new HTTPRedirectDeflateDecoder();
+		decoder.setHttpServletRequest(req);
+
+		AuthnRequest authnRequest;
+		try {
+			decoder.initialize();
+
+			decoder.decode();
+			MessageContext messageContext = decoder.getMessageContext();
+			authnRequest = (AuthnRequest) messageContext.getMessage();
+
+		} catch (ComponentInitializationException | MessageDecodingException e) {
+			throw new RuntimeException(e);
+		}
+
+		logger.info("AuthnRequest recieved");
+		logger.info("AuthnRequest redirect URL: ");
+		logger.info(req.getRequestURL().toString() + "?" + req.getQueryString());
+		logger.info("AuthnRequest message: ");
+		OpenSAMLUtils.logSAMLObject(authnRequest);
+
+		Writer w = resp.getWriter();
+		resp.setContentType("text/html");
+		w.append("<html>" + "<head></head>" + "<body><h1>AuthnRequest received. Results in the console log</h1>"
+				+ "</body>" + "</html>");
+	}
+
+	@Override
+	protected void doPost(final HttpServletRequest req, final HttpServletResponse resp)
+			throws ServletException, IOException {
+	}
+
+}
